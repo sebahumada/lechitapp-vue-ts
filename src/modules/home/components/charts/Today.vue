41e3f6b5fa3d8796134ebc4a7435332d09583b0a
Sebastian Ahumada <cbasmx@gmail.com>
2022-04-12 17:19:03 -0400

    font family

--- src/modules/home/components/charts/Today.vue
+++ src/modules/home/components/charts/Today.vue
@@ -1,49 +1,50 @@
 <template>
-    <h3>Hoy</h3>
-    
-    <Line
-    :chart-options="chartOptions"
-    :chart-data="chartData"
-    :chart-id="chartId"
-    :dataset-id-key="datasetIdKey"
-    :width="400"
-    :height="150"
-  />
+    <div class="mb-4">
+        <h3>Hoy</h3>
 
+        <Line
+            :chart-options="chartOptions"
+            :chart-data="chartData"
+            :chart-id="chartId"
+            :dataset-id-key="datasetIdKey"
+            :width="400"
+            :height="150"
+        />
+    </div>
 </template>
 
 
 <script lang="ts" setup>
 import { Line } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement} from 'chart.js'
+import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement } from 'chart.js'
 import { Register } from '../../../../interfaces/interfaces';
 
 ChartJS.register(Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement);
 
 
-const props = defineProps<{  
-  registers : Register[]  
+const props = defineProps<{
+    registers: Register[]
 }>()
 
-const horas = Array.from(props.registers, x=>x.hora);
-const cantidades = Array.from(props.registers, x=>x.cantidad);
+const horas = Array.from(props.registers, x => x.hora);
+const cantidades = Array.from(props.registers, x => x.cantidad);
 
 
 
 const chartOptions = { responsive: true }
 const chartData = {
-      labels: horas,
-      datasets: [ 
-            { 
-                label: 'ml. leche',
-                backgroundColor: '#f87979',
-                borderColor: '#f87979',
-                data: cantidades,
-                fill: true
+    labels: horas,
+    datasets: [
+        {
+            label: 'ml. leche',
+            backgroundColor: '#f87979',
+            borderColor: '#f87979',
+            data: cantidades,
+            fill: true
 
-            } 
-              ]
-    }
+        }
+    ]
+}
 
 const chartId = 'bar-chart';
 const datasetIdKey = 'label';
--- src/modules/home/layouts/HomeLayout.vue
+++ src/modules/home/layouts/HomeLayout.vue
@@ -91,7 +91,8 @@ a{
 
 
 #contenido, .offcanvas-body, .offcanvas-header{
-    font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif
+    font-family: 'Roboto', sans-serif;
+    
 }
 
 
